ba8b24f903a5ecf86321439c288c8dbc8a2811c0
Luca Semprini <luca.semprini10@studio.unibo.it>
2018-04-20 18:20:11 +0200

    GUI creata

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import view.ViewController;
+
+import java.io.IOException;
+
+public class Main extends Application{
+
+    private static final String LAYOUT_PATH = "/view/view.fxml";
+    private static final String WINDOW_TITLE = "REGEXP MATCHING TOOL";
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final FXMLLoader loader = initGui(primaryStage);
+        final ViewController initialWindow = loader.getController();
+        primaryStage.show();
+    }
+
+    /**
+     * Metodo per inizializzare la GUI dell'applicazione.
+     * @param primaryStage lo stage primario della GUI JavaFX.
+     * @return il FXMLLoader.
+     */
+    private FXMLLoader initGui(final Stage primaryStage) throws IOException {
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource(LAYOUT_PATH));
+        final Parent root = loader.load();
+        primaryStage.setTitle(WINDOW_TITLE);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setOnCloseRequest(event -> {
+            Platform.exit();
+            System.exit(0);
+        });
+        return loader;
+    }
+}
---
+++ src/view/ViewController.java
@@ -0,0 +1,14 @@
+package view;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ViewController {
+    public TextField maxDepthField;
+    public TextField pathField;
+    public Button buttonSearch;
+    public Label presentationLabel;
+    public Label pathLabel;
+    public Label maxDepthLabel;
+}
